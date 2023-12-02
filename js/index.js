0f5f28651990a419f346c423f7cf9b9120dccaa6
Richard-Younes <richard.younes@lau.edu>
2023-12-02 19:53:50 +0200

    first commit

---
+++ js/index.js
@@ -0,0 +1,338 @@
+/** @format */
+"Use strict";
+// Binary matrix representing the maze
+let mazeMatrix = [
+	[
+		1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+		1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+	],
+	[
+		1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+		0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1,
+	],
+	[
+		1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1,
+		1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1,
+	],
+	[
+		1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0,
+		0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1,
+	], //1 number 7 is set to 0 for animation purposes
+	[
+		1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0,
+		1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1,
+	],
+	[
+		1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0,
+		1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1,
+	],
+	[
+		1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0,
+		1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1,
+	],
+	[
+		1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0,
+		0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1,
+	],
+	[
+		1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1,
+		1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1,
+	],
+	[
+		1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0,
+		1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1,
+	],
+	[
+		1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0,
+		1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1,
+	],
+	[
+		1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0,
+		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1,
+	],
+	[
+		1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0,
+		1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1,
+	],
+	[
+		1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+		1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1,
+	],
+	[
+		1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0,
+		1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1,
+	],
+	[
+		1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0,
+		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1,
+	],
+	[
+		1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0,
+		1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1,
+	], // the 2nd 1 is now 0 due to mapping things
+	[
+		1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0,
+		1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1,
+	],
+	[
+		1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0,
+		1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1,
+	],
+	[
+		1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0,
+		0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1,
+	],
+	[
+		1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0,
+		1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1,
+	],
+	[
+		1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0,
+		1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1,
+	],
+	[
+		1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1,
+		1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1,
+	],
+	[
+		1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0,
+		1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1,
+	],
+	[
+		1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0,
+		1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1,
+	],
+	[
+		1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0,
+		1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1,
+	],
+	[
+		1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1,
+		1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1,
+	],
+	[
+		1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0,
+		1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1,
+	],
+	[
+		1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0,
+		1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1,
+	],
+	[
+		1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0,
+		0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1,
+	],
+	[
+		1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0,
+		1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1,
+	],
+	[
+		1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0,
+		1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1,
+	],
+	[
+		1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0,
+		1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1,
+	],
+	[
+		1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0,
+		1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1,
+	],
+	[
+		1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1,
+		1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1,
+	],
+	[
+		1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0,
+		1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1,
+	],
+	[
+		1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0,
+		1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1,
+	],
+	[
+		1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0,
+		1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1,
+	],
+	[
+		1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0,
+		1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1,
+	],
+	[
+		1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0,
+		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1,
+	],
+	[
+		1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+		1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+	],
+];
+
+let size = 5;
+let textures = [
+	"images/walls1.jpeg",
+	"images/walls4.jpeg",
+	"images/walls3.jpeg",
+	"images/walls2.jpeg",
+]; // Add your texture file paths here
+let normalMaps = [
+	"images/normal-map-walls1.png",
+	"images/normal-map-walls4.png",
+	"images/normal-map-walls3.png",
+	"images/normal-map-walls2.png",
+]; // Add your normal map file paths here
+
+// Define the boundaries of each section
+let sections = [
+	{
+		start: 0,
+		end: 11,
+	}, // Section 1
+	{
+		start: 12,
+		end: 22,
+	}, // Section 2
+	{
+		start: 23,
+		end: 32,
+	}, // Section 3
+	{
+		start: 33,
+		end: 41,
+	}, // Section 4
+];
+
+// Iterate over the matrix to create the maze
+for (let i = 0; i < mazeMatrix.length; i++) {
+	for (let j = 0; j < mazeMatrix[i].length; j++) {
+		// If the matrix element is 1, create a box entity at the corresponding position
+		if (mazeMatrix[i][j] === 1) {
+			let box = document.createElement("a-box");
+			box.setAttribute("position", {
+				x: j * size,
+				y: size / 2,
+				z: -i * size,
+			});
+			box.setAttribute("width", size);
+			box.setAttribute("height", size);
+			box.setAttribute("depth", size);
+			box.setAttribute("static-body", "");
+
+			// Set the texture and normal map for each section of the maze
+			let textureIndex = 0;
+			for (let k = 0; k < sections.length; k++) {
+				if (i >= sections[k].start && i <= sections[k].end) {
+					textureIndex = k % textures.length;
+					break;
+				}
+			}
+			box.setAttribute("material", "src", textures[textureIndex]);
+			box.setAttribute("material", "repeat", "1 1"); // Repeat the color texture 2 times in both x and y directions
+			box.setAttribute("material", "normalMap", normalMaps[textureIndex]);
+			box.setAttribute("material", "normalScale", "2 2"); // Repeat the normal map 2 times in both x and y directions
+
+			document.querySelector("a-scene").appendChild(box);
+		}
+	}
+}
+
+AFRAME.registerComponent("move-on-click", {
+	schema: {
+		target: { type: "selector" },
+		destination: { type: "vec3" },
+	},
+	init: function () {
+		var data = this.data;
+		this.el.addEventListener("click", function () {
+			data.target.setAttribute("animation", {
+				property: "position",
+				to: data.destination,
+				dur: 2000, // Duration of the animation in milliseconds
+				easing: "easeInOutQuad", // Easing function
+			});
+		});
+	},
+});
+
+// Get the sphere element
+let sphere = document.getElementById("mySphere");
+let count = 0;
+// Animate the sphere's position
+
+// timer
+
+let counterValue = 0;
+const counterDisplay = document.getElementById("counterDisplay");
+
+function updateCounter() {
+	counterValue++;
+	counterDisplay.textContent = "Time: " + counterValue;
+
+	// Check if you've reached the end of the map, and clear the interval if necessary
+	if (counterValue >= mapEnd) {
+		clearInterval(intervalId);
+	}
+}
+
+function resetCounter() {
+	counterValue = 0;
+	counterDisplay.textContent = "Time: " + counterValue;
+}
+
+// Set the interval to update the counter every second (1000 milliseconds)
+
+// Set the end of the map value (change this to your actual end value)
+const mapEnd = Infinity; // Replace 10 with your actual end value
+window.intervalId = setInterval(updateCounter, 1000);
+AFRAME.registerComponent("stop-counter", {
+	init: function () {
+		this.el.addEventListener("click", function () {
+			console.log("GLTF model clicked");
+			clearInterval(window.intervalId);
+		});
+	},
+});
+
+function moveBoxes() {
+	// Remove the event listener to prevent multiple clicks
+	document.getElementById("Panel").removeAttribute("onclick");
+
+	// Array of box IDs
+	var boxIds = [
+		"myBox6",
+		"myBox7",
+		"myBox8",
+		"myBox9",
+		"myBox10",
+		"myBox11",
+		"myBox12",
+		"myBox13",
+		"myBox14",
+		"myBox15",
+		"myBox16",
+		"myBox17",
+		"myBox18",
+	];
+
+	// Function to move a box with a delay
+	function moveBoxWithDelay(index) {
+		if (index < boxIds.length) {
+			setTimeout(function () {
+				var box = document.getElementById(boxIds[index]);
+				box.setAttribute("animation", {
+					property: "position",
+					to: { x: box.getAttribute("position").x, y: 2.5, z: -85 },
+					dur: 2000, // Duration of the animation in milliseconds (2 seconds)
+				});
+
+				// Move the next box after the current animation completes
+				box.addEventListener("animationcomplete", function () {
+					moveBoxWithDelay(index + 1);
+				});
+			}, index * 200); // Add a delay of 2 seconds for each box
+		}
+	}
+
+	// Start moving boxes with a delay
+	moveBoxWithDelay(0);
+}
