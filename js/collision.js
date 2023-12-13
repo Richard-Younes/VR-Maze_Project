3d5a7a333da6f3c8450157fc1a82864510ec8d74
Luidovick <luidoviczgheib22@gmail.com>
2023-12-13 18:26:45 +0200

    Almost Done

--- js/collision.js
+++ js/collision.js
@@ -1,15 +1,15 @@
 /** @format */
-'Use strict';
+"Use strict";
 
 // timer
 
 let counterValue = 0;
-const counterDisplay = document.getElementById('counterDisplay');
+const counterDisplay = document.getElementById("counterDisplay");
 let arrived = true;
 function updateCounter() {
 	if (!arrived) {
 		counterValue++;
-		counterDisplay.textContent = 'Time: ' + counterValue;
+		counterDisplay.textContent = "Time: " + counterValue;
 
 		// Check if you've reached the end of the map, and clear the interval if necessary
 		if (counterValue >= mapEnd) {
@@ -20,7 +20,7 @@ function updateCounter() {
 
 function resetCounter() {
 	counterValue = 0;
-	counterDisplay.textContent = 'Time: ' + counterValue;
+	counterDisplay.textContent = "Time: " + counterValue;
 }
 
 // Set the interval to update the counter every second (1000 milliseconds)
@@ -30,12 +30,13 @@ const mapEnd = Infinity; // Replace 10 with your actual end value
 window.intervalId = setInterval(updateCounter, 1000);
 
 // The following is the lives part of the code
-let playerLives = 4;
+let playerLives = 1;
 
-const livesDisplay = document.getElementById('player-lives-display');
+console.log(document);
+let livesDisplay = document.getElementById("player-lives-display");
 
 for (let i = 0; i < playerLives; i++) {
-	livesDisplay.textContent += '❤️';
+	livesDisplay.textContent += "❤️";
 }
 
 console.log(playerLives);
@@ -43,41 +44,41 @@ console.log(playerLives);
 // *********************Handle the collision**********************************//
 
 // Get the player entity by its ID
-const player = document.getElementById('player');
+const player = document.getElementById("player");
 const beginPosition = { x: 5, y: 2.1, z: 10 }; // Set the new position values
-const ghosts = document.querySelectorAll('.ghost'); // Get all entities with the class 'ghost'
+const ghosts = document.querySelectorAll(".ghost"); // Get all entities with the class 'ghost'
 
 const margin = 0.8;
 
 function checkCollision() {
 	// Get the position of the player entity
-	const playerPosition = player.getAttribute('position');
+	const playerPosition = player.getAttribute("position");
 
 	// Iterate through all entities with the class 'ghost'
 	ghosts.forEach((ghost) => {
 		// Get the position of the current ghost entity
-		const ghostPosition = ghost.getAttribute('position');
+		const ghostPosition = ghost.getAttribute("position");
 
 		if (playerLives > 0) {
 			// Check for collision with the current ghost
 			if (Math.abs(playerPosition.x - ghostPosition.x) <= margin && Math.abs(playerPosition.z - ghostPosition.z) <= margin) {
 				// Log the player position
-				console.log('Player Position:', playerPosition);
+				console.log("Player Position:", playerPosition);
 
 				// Log the ghost position
-				console.log('Ghost Position:', ghostPosition);
+				console.log("Ghost Position:", ghostPosition);
 
-				console.log('Collision');
+				console.log("Collision");
 				playerLives = playerLives - 1;
-				livesDisplay.textContent = '';
+				livesDisplay.textContent = "";
 				for (let i = 0; i < playerLives; i++) {
-					livesDisplay.textContent += '❤️';
+					livesDisplay.textContent += "❤️";
 				}
 				console.log(playerLives);
-				player.setAttribute('position', beginPosition);
+				player.setAttribute("position", beginPosition);
 			}
 		} else {
-			console.log('You Lost');
+			console.log("You Lost");
 			showGameOverScreen();
 		}
 	});
@@ -87,23 +88,67 @@ function checkCollision() {
 setInterval(checkCollision, 100); // Adjust the interval as needed
 
 function showGameOverScreen() {
-	document.querySelector('.game-container').style.display = 'none';
-	document.querySelector('.game-over').style.display = 'block';
+	document.querySelector(".game-container").style.display = "none";
+	document.querySelector(".game-over").style.display = "block";
 }
 
-// Collision with the ew game image
+// Collision with the new game image
 
-const newGameImg = document.querySelector('.new-game-img');
-const newGame = document.getElementById('new-game');
+const newGameImg = document.querySelector(".new-game-img");
+const newGame = document.getElementById("new-game");
 console.log(newGame);
 function checkCollisionNewGame() {
-	const playerPosition = player.getAttribute('position');
-	const imgCollision = newGameImg.getAttribute('position');
+	const playerPosition = player.getAttribute("position");
+	const imgCollision = newGameImg.getAttribute("position");
 
-	if (playerPosition.z - imgCollision.z <= 1 && playerPosition.y > 35) {
-		player.setAttribute('position', { x: 5, y: 39, z: 15 });
+	if (playerPosition.z - imgCollision.z <= 1.5 && playerPosition.y > 35) {
+		player.setAttribute("position", { x: 5, y: 39, z: 12 });
 		arrived = false;
 		newGame.remove();
 	}
 }
 setInterval(checkCollisionNewGame, 100); // Adjust the interval as needed
+
+// Winnig condition
+// Collision With the trophy
+
+const trophy = document.querySelector(".trophy");
+const trophyPosition = trophy.getAttribute("position");
+const highscoreDisplay = document.getElementById("highscore");
+let fireworks = document.querySelector("#fireworks");
+let launchSound = document.querySelector("#launch");
+let burstSounds = [document.querySelector("#burst1"), document.querySelector("#burst2"), document.querySelector("#burst3")];
+let highscore = 9999999;
+let gameWon = false;
+function winGame() {
+	let playerPosition = player.getAttribute("position");
+	if (Math.abs(playerPosition.x - trophyPosition.x) <= 1 && Math.abs(playerPosition.z - trophyPosition.z) <= 1) {
+		console.log("WIIIIIIIIIIIIIIIIIIIIIIIIIINNNNNNNNNNNNNNNNNNNNNNNNNNN");
+		arrived = true;
+		gameWon = true;
+
+		if (highscore > counterValue) {
+			highscore = counterValue;
+			console.log(highscore);
+		}
+		highscoreDisplay.textContent = "Highscore: " + highscore;
+
+		// Fireworks
+
+		launchSound.play();
+		if (gameWon) {
+			animateFireworks();
+			gameWon = false;
+		}
+
+		let burstSound = burstSounds[Math.floor(Math.random() * burstSounds.length)];
+		burstSound.play();
+	}
+}
+// function animateFireworks() {
+// 	fireworks.setAttribute("particle-system", "preset: star; color: #FF0000, #FFFF00; particleCount: 10; size: 0.1; maxAge: 1");
+// 	requestAnimationFrame(animateFireworks);
+// }
+
+// Set up the interval
+setInterval(winGame, 100);
